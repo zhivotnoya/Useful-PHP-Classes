17e9373e1b2a9cabcb547cab24aabd88ba83534c
Thomas Kroll <zhivotnoya@gmail.com>
2017-12-11 05:49:42 -0500

    Update template.class.php
    An easier, bug-free version.

--- template.class.php
+++ template.class.php
@@ -1,69 +1,95 @@
 <?php
-	// template.class.php
-	
-	/*  this doesn't handle complex templates, or have any logic added, yet. */
-	
-	class Template
-	{
-	
-		private $template;
-		
-		function __construct ($template = null)
-		{
-			if (isset($template))
-			{
-				$this->load($template);
-			}
-		}
-		
-		function __destruct()
-		{
-			// destructions stuff here...if needbe
-		}
-		
-		public function load($file)
-		{
-			if (isset($file) && file_exists($file))
-			{
-				$this->template = file_get_contents($file);	
-			}
-		}
-		
-		public function set($var, $content)
-		{
-			$this->template = str_replace("{@". $var ."}", $content, $this->template);
-		}
-		
-		public function publish()
-		{
-			/* 
-			 * Prints out the theme to the page
-			 * However, before we do that, we need to remove every var
-			 * within {} that are not set
-			 */
-			 
-			$this->removeEmpty();
-			/* eval("<?php" . $this->template . "?>"); */
-			
-			return $this->template;
-			
-		}
-		
-		private function removeEmpty()
-		{
-			/*
-			 * this function would remove all empty variables from the template wrapped in {}
-			 */
-			
-			$this->template = preg_replace('^{.*}^', "", $this->template);
-		}
-		
-		public function parse()
-		{
-			// this function grabs a static document and returns the content
-			return $this->template;
-		}
-		
-	}
+
+	/**
+	 * Simple template engine class (use [@tag] tags in your templates).
+	 * 
+	 * @link http://www.broculos.net/ Broculos.net Programming Tutorials
+	 * @author Nuno Freitas <nunofreitas@gmail.com>
+	 * @version 1.0
+	 */
+    class Template {
+    	/**
+    	 * The filename of the template to load.
+    	 *
+    	 * @access protected
+    	 * @var string
+    	 */
+        protected $file;
+        
+        /**
+         * An array of values for replacing each tag on the template (the key for each value is its corresponding tag).
+         *
+         * @access protected
+         * @var array
+         */
+        protected $values = array();
+        
+        /**
+         * Creates a new Template object and sets its associated file.
+         *
+         * @param string $file the filename of the template to load
+         */
+        public function __construct($file) {
+            $this->file = $file;
+        }
+        
+        /**
+         * Sets a value for replacing a specific tag.
+         *
+         * @param string $key the name of the tag to replace
+         * @param string $value the value to replace
+         */
+        public function set($key, $value) {
+            $this->values[$key] = $value;
+        }
+        
+        /**
+         * Outputs the content of the template, replacing the keys for its respective values.
+         *
+         * @return string
+         */
+        public function publish() {
+        	/**
+        	 * Tries to verify if the file exists.
+        	 * If it doesn't return with an error message.
+        	 * Anything else loads the file contents and loops through the array replacing every key for its value.
+        	 */
+            if (!file_exists($this->file)) {
+            	return "Error loading template file ($this->file).<br />";
+            }
+            $output = file_get_contents($this->file);
+            
+            foreach ($this->values as $key => $value) {
+            	$tagToReplace = "[@$key]";
+            	$output = str_replace($tagToReplace, $value, $output);
+            }
+
+            return $output;
+        }
+        
+        /**
+         * Merges the content from an array of templates and separates it with $separator.
+         *
+         * @param array $templates an array of Template objects to merge
+         * @param string $separator the string that is used between each Template object
+         * @return string
+         */
+        static public function merge($templates, $separator = "\n") {
+        	/**
+        	 * Loops through the array concatenating the outputs from each template, separating with $separator.
+        	 * If a type different from Template is found we provide an error message. 
+        	 */
+            $output = "";
+            
+            foreach ($templates as $template) {
+            	$content = (get_class($template) !== "Template")
+            		? "Error, incorrect type - expected Template."
+            		: $template->output();
+            	$output .= $content . $separator;
+            }
+            
+            return $output;
+        }
+    }
 
 ?>
